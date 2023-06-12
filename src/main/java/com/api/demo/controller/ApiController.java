db67f96f2d665f03ae1ff046eca0a6aa6f7b2320
jskim5 <jskim5@enliple.com>
2023-06-12 13:00:45 +0900

    컨트롤러 예시

--- src/main/java/com/api/demo/controller/ApiController.java
+++ src/main/java/com/api/demo/controller/ApiController.java
@@ -2,13 +2,18 @@ package com.api.demo.controller;
 
 import com.api.demo.domain.user.UserDto;
 import com.api.demo.service.ApiService;
+import com.api.demo.util.Constant;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -34,5 +39,31 @@ public class ApiController {
         UserDto userDto = (UserDto) jsonPlaceHolderApiService.getOne(id);
 
         return ResponseEntity.ok(userDto);
+    }
+
+    @GetMapping("/api/test")
+    public ResponseEntity<?> test(
+            HttpServletRequest request
+    ){
+        log.info("{} {} 진입", request.getRequestURL(), request.getMethod());
+
+        WebClient webClient = WebClient.create(Constant.JSON_PLACEHOLDER_USER_API_URL+Constant.JSON_PLACEHOLDER_USER_API_URI);
+        List<UserDto> list = webClient.get()
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(UserDto.class)
+                .collectList()
+                .block();
+
+        WebClient webClient2 = WebClient.create(Constant.JSON_PLACEHOLDER_USER_API_URL+Constant.JSON_PLACEHOLDER_USER_API_URI+"/1");
+        UserDto userDto = webClient2.get()
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(UserDto.class)
+                .block();
+
+        System.out.println(list.toString());
+        System.out.println(userDto.toString());
+        return ResponseEntity.ok("");
     }
 }
